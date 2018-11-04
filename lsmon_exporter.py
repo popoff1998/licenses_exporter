5ec5b733325be3f12a513740dfbe07447615d120
tonin <tonin@uco.es>
2018-11-04 12:23:59 +0100

    Version que incluye lsmon y lmutil

--- lsmon_exporter.py
+++ lsmon_exporter.py
@@ -6,9 +6,12 @@ Tonin 2018
 import commands
 from prometheus_client import Gauge, start_http_server
 import time
+import re
+import ruamel.yaml as yaml
 
 #Algunas globales para controlar el programa
 CMD = './lsmon'
+LICENSES_FILE = 'licenses.yml'
 SERVER = 'windhcp2'
 PORT = 8000
 SLEEP = 5
@@ -21,88 +24,156 @@ Apps =  {
 class User(object):
     def __init__(self, name):
         self.name = name
-        self.hostName = ''
+        self.hostName = None
+        self.device = None
+        self.date = None
+
+    def printUser(self):
+        print "\t",user.name," ",user.hostName," ",user.device," ",user.date
 
 class Feature(object):
-    def __init__(self, name, fqdn):
-        self.name = name.replace('"','')
-        self.fqdn = fqdn
+    def __init__(self, name, app):
+        self.name = name
         self.maxLicenses = 0
         self.inUse = 0
+        self.app = app
         self.userList = []
-        try:
-            self.app = Apps[self.name][0]
-        except KeyError:
-            self.app = None
+
+        if(self.app == 'lsmon'):
+            try:
+                self.app = Apps[self.name][0]
+            except KeyError:
+                self.app = None
 
     def printFeature(self):
         print "app=",self.app," feature=",self.name," max=",self.maxLicenses," current=",self.inUse
         for user in self.userList:
-            print "\t",user.name," ",user.hostName
+            user.printUser()
 
-class Features(object):
-    #de momento lo hago monoservidor
-    def __init__(self, fqdn):
-        self.fqdn = fqdn
+class App(object):
+    def __init__(self, parent, name, server, type, include):
+        self.parent = parent
+        self.name = name
+        self.server = server
+        self.type = type
+        self.include = include
         self.featureList = []
-        self.flexlm_feature_issued = Gauge('flexlm_feature_issued',"dummy",['app','name'])
-        self.flexlm_feature_used = Gauge('flexlm_feature_used',"dummy",['app','name'])
-        self.flexlm_feature_used_users = Gauge('flexlm_feature_used_users','dummy',['app','name','user'])
-        self.flexlm_server_status = Gauge('flexlm_server_status','dummy',['app','fqdn','master','port','version'])
+        if(self.type == 'lsmon'):
+            self.parse = self.parseLsmon
+        elif(self.type == 'lmutil'):
+            self.parse = self.parseLmutil
+        """
+            No se muy bien como comprobar de forma generica si esta online.
+            Parece que si al parsear lo encuentra es porque esta online, pero tengo
+            que hacer mas comprobaciones de como se comporta en grafanaself.
+        """
+        self.online = True
 
-    def parse(self):
-        output = commands.getstatusoutput(CMD + ' ' + self.fqdn)
+    def parseLsmon(self):
+        self.featureList = []
+        output = commands.getstatusoutput(self.parent.LSMONCMD + ' ' + self.server)
         for _output in output:
             if type(_output) == type(''):
                 lines = _output.split('\n')
                 for line in lines:
                     if 'Feature name' in line:
-                        feature = Feature(line.split(":",1)[1][1:-3], self.fqdn)
-                        self.featureList.append(feature)
+                        aux = line.split(":",1)[1][1:-3].replace('"','')
+                        if aux in str(self.include):
+                            feature = Feature(aux, self.name)
+                            self.featureList.append(feature)
+                        else:
+                            feature = None
                     if 'Maximum concurrent user' in line:
-                        feature.maxLicenses = float(line.split(":",1)[1][1:])
+                        if(feature): feature.maxLicenses = float(line.split(":",1)[1][1:])
                     if 'Unreserved tokens in use' in line:
-                        feature.inUse = float(line.split(":",1)[1][1:])
+                        if(feature): feature.inUse = float(line.split(":",1)[1][1:])
                     if 'User name' in line:
-                        user = User(line.split(":",1)[1][1:])
-                        feature.userList.append(user)
+                        if(feature):
+                            user = User(line.split(":",1)[1][1:])
+                            feature.userList.append(user)
                     if 'Host name' in line:
-                        user.hostName =  line.split(":",1)[1][1:]
+                        if(feature): user.hostName =  line.split(":",1)[1][1:]
 
-    def isOnline(self, app):
-        for feature in self.featureList:
-            if app == feature.name:
-                return 1
-        return 0
+    def parseLmutil(self):
+        self.featureList = []
+        output = commands.getstatusoutput(self.parent.LMUTILCMD + ' ' + self.server)
+        for _output in output:
+            if type(_output) == type(''):
+                lines = _output.split('\n')
+                for line in lines:
+                    if 'Users of' in line:
+                        r = re.search('Users of (.*):  \(Total of (.*)licenses? issued;  Total of (.*) licenses? in use\)',line)
+                        feature = Feature(r.group(1), self.name)
+                        feature.maxLicenses = float(r.group(2))
+                        feature.inUse = float(r.group(3))
+                        self.featureList.append(feature)
+                    if ', start' in line:
+                        r = re.search('^\s+(.*) (.*) (.*) \((.*)\) \((.*)/(.*) (.*)\), start (.*)',line)
+                        user = User(r.group(1))
+                        user.hostName = r.group(2)
+                        user.device = r.group(3)
+                        user.date = r.group(8)
+                        feature.userList.append(user)
 
     def printFeatures(self):
         for feature in self.featureList:
             feature.printFeature()
 
     def updateMetric(self):
-        features.parse()
+        self.parse()
+        self.parent.flexlm_server_status.labels(app=self.name,fqdn=self.server,
+                                         master='true',port='port',
+                                         version='version').set(self.online)
+
         for feature in self.featureList:
-            if feature.app:
-                self.flexlm_feature_issued.labels(app=feature.app,name=feature.name).set(feature.maxLicenses)
-                self.flexlm_feature_used.labels(app=feature.app,name=feature.name).set(feature.inUse)
-                for user in feature.userList:
-                    self.flexlm_feature_used_users.labels(app=feature.app,name=feature.name,user=user.name).set(1)
-        """
-            lsmon es un tanto diferente de lmutil da solo info
-            de las fetures que estan ejecutandose, por lo que hay que
-            comprobar el estado de una licencia en base a conocimiento
-            previo de las que deben de estar. Para ello usamos el diccionario
-            hardcoded. Mas adelante lo leeremos de un yml
-        """
-        for app,value in Apps.iteritems():
-            self.flexlm_server_status.labels(app=value[0],fqdn=value[1],
-                                             master='true',port='5093',
-                                             version=value[2]).set(self.isOnline(app))
+            self.parent.flexlm_feature_issued.labels(app=feature.app,name=feature.name).set(feature.maxLicenses)
+            self.parent.flexlm_feature_used.labels(app=feature.app,name=feature.name).set(feature.inUse)
+
+            for user in feature.userList:
+                self.parent.flexlm_feature_used_users.labels(app=feature.app,name=feature.name,user=user.name,
+                                                             host=user.hostName,device=user.device).set(1)
+
+class Apps(object):
+    def __init__(self):
+        self.appList = []
+
+        with open(LICENSES_FILE, 'r') as fp:
+            self.cfg = yaml.safe_load(fp)
+        for appCfg in self.cfg['licenses']:
+            app = App(self, appCfg['name'], appCfg['license_server'], appCfg['type'], appCfg['features_to_include'])
+            self.appList.append(app)
+
+        self.flexlm_feature_used = Gauge('flexlm_feature_used',"dummy",['app','name'])
+        self.flexlm_feature_issued = Gauge('flexlm_feature_issued',"dummy",['app','name'])
+        self.flexlm_feature_used_users = Gauge('flexlm_feature_used_users','dummy',['app','name','user','host','device'])
+        self.flexlm_server_status = Gauge('flexlm_server_status','dummy',['app','fqdn','master','port','version'])
+        self.PORT = self.cfg['config']['port']
+        self.SLEEP = self.cfg['config']['sleep']
+        self.LSMONCMD = self.cfg['config']['lsmon_cmd']
+        self.LMUTILCMD = self.cfg['config']['lmutil_cmd']
+
+    def parse(self):
+        for app in self.appList:
+            app.parse()
+
+    def printApps(self):
+        for app in self.appList:
+            app.printFeatures()
+
+    def updateMetric(self):
+        self.flexlm_feature_used_users._metrics.clear()
+        for app in self.appList:
+            app.updateMetric()
+"""
+apps = Apps()
+apps.parse()
+apps.printApps()
+"""
 
 
 if __name__ == '__main__':
-    features = Features(SERVER)
-    start_http_server(PORT)
+    apps = Apps()
+    start_http_server(apps.PORT)
     while True:
-        features.updateMetric()
-        time.sleep(SLEEP)
+        apps.updateMetric()
+        time.sleep(apps.SLEEP)
